134916aef66be3c6d3c028efd5d5a333de531e1b
wilsto <willy.stophe.pro@gmail.com>
2016-02-12 22:51:41 +0100

    v0.1.1
    test github + slack

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,12 +12,12 @@ interface Hero {
 template:`
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
-<ul class="heroes">
-<li *ngFor="#hero of heroes" (click)="onSelect(hero)">
-  <span class="badge">{{hero.id}}</span> {{hero.name}}
-</li>
-</ul>
-<div *ngIf="selectedHero">
+	<ul class="heroes">
+	<li *ngFor="#hero of heroes" (click)="onSelect(hero)">
+	  <span class="badge">{{hero.id}}</span> {{hero.name}}
+	</li>
+	</ul>
+	<div *ngIf="selectedHero">
       <h2>{{selectedHero.name}} details!</h2>
       <div><label>id: </label>{{selectedHero.id}}</div>
       <div>
